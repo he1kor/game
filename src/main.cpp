43660fcc6e61189b2d68a48e9b664178ea9e286f
Helkor <helkor.rightway@gmail.com>
2023-04-03 08:54:23 +0300

    "Up key" exits, game.exe now launches without IDE

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,77 @@
 #include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <SDL.h>
-#include <GL/gl.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-int main( int argc, char* args[] ) {
-    //The window we'll be rendering to
-    SDL_Window *window = NULL;
+const GLint WIDTH = 800, HEIGHT = 600;
 
-    //The surface contained by the window
-    SDL_Surface *screenSurface = NULL;
+bool quit = false;
 
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    } else {
-        //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        }
-        SDL_Event e;
-        bool quit = false;
-        while (!quit) {
-            while (SDL_PollEvent(&e) != 0) {
-                if (e.type == SDL_QUIT) {
-                    quit = true;
-                }
-            } // end of handling event.
-        }
+void processKey(SDL_Keysym keysym)
+{
+    switch (keysym.sym)
+    {
+        case SDLK_UP:
+            quit = true;
     }
-    SDL_DestroyWindow( window );
+}
 
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,3);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    SDL_Window *window = SDL_CreateWindow("Minecraft", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    glewExperimental = GL_TRUE;
+
+    if (GLEW_OK != glewInit())
+    {
+        std::cout << "Failed to initialize GLEW";
+
+        return EXIT_FAILURE;
+    }
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    SDL_Event windowEvent;
+
+    while (true)
+    {
+        while (SDL_PollEvent(&windowEvent))
+        {
+            switch (windowEvent.type)
+            {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_KEYDOWN:
+                    processKey(windowEvent.key.keysym);
+                    break;
+            }
+            if (quit) goto ExitProgram;
+        }
+        SDL_Delay(20);
+
+        glClearColor(1.12f, 1.1f, 1.12f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+    ExitProgram:
+
+    SDL_GL_DeleteContext(context);
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
-    return 0;
+    return EXIT_SUCCESS;
+
 }
